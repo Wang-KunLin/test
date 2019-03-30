6e6a69e4ca9a08f6688edfeb313f80edc31ff3ae
Wang-KunLin <39510454+Wang-KunLin@users.noreply.github.com>
2019-03-30 14:20:15 +0800

    zhihu

--- newspider/now0330.py
+++ newspider/now0330.py
@@ -2,8 +2,13 @@
 # -*- coding:utf-8 -*-
 
 import requests
+import re
 
-url = "https://baidu.com/robot.txt"
-rp = requests.get(url)
-html = rp.text
-print(html)
+url = "https://www.zhihu.com/explore"
+header = {"User-Agent": '''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)
+ Chrome/72.0.3626.121 Safari/537.36'''}
+rp = requests.get(url, header)
+html = re.compile('explore-feed.*?question_link.*?>(.*?)</a>', re.S)
+title = re.findall(html, rp.text)
+print(title)
+
